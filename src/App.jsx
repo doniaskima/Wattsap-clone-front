2ef79652809124573c37fbd6fd9e4958c97900bb
unknown <doniaskima344@gmail.com>
2023-01-24 22:35:50 +0100

    Loader Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css"
 import "./styles/main.scss"
 function App() {
   return (
-    <div className="container">
+    <div className="App">
       <LoaderPage />
     </div>
   )
--- src/Pages/LoaderPage.jsx
+++ src/Pages/LoaderPage.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import Icon from "../Components/LoaderPage/Icon";
 
-const LoaderPage = ({done}) => {
+const LoaderPage = ({ done }) => {
     return (
-        <div className="loaderPage">
-            <div className="logoWrapper">
-                <Icon className="loader-logo"/>
+         
+            <div className="loader">
+                <div className="loader__logo-wrapper">
+                    <Icon id="whatsapp" className="loader__logo" />
+                </div>
+                <div
+                    className={`loader__progress ${done ? "loader__progress--done" : ""}`}
+                ></div>
+                <h1 className="loader__title"> Whatsapp</h1>
+                <p className="loader__desc">
+                    <Icon id="lock" className="loader__icon" />
+                    End-to-end encrypted.  
+                </p>
             </div>
-            <div
-                className={`loader__progress ${done ? "loader__progress--done" : ""}`}
-            ></div>
-            <h1 className="loader-title">Whatsapp</h1>
-            <p className="loaderDesc">
-                <Icon id="lock" className="loader__icon" />
-                End-to-end encrypted.
-            </p>
-        </div>
-    )
-}
+        
+ 
+    );
+};
 
 export default LoaderPage
