d5601774bfe822f4c01ad48fae1f4797d2d18968
unknown <doniaskima344@gmail.com>
2023-01-24 20:39:59 +0100

    add css

--- src/Pages/LoaderPage.jsx
+++ src/Pages/LoaderPage.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+
 
 const LoaderPage = () => {
     return (
         <div className="loaderPage">
             <div className="logoWrapper">
-                
+
             </div>
         </div>
     )
