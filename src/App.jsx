f83cf757ded2158508633a828b7289a1ee0b5b9f
unknown <doniaskima344@gmail.com>
2023-01-24 20:37:36 +0100

    add icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import './App.css'
+import LoaderPage from "./Pages/LoaderPage";
 
 function App() {
   return (
     <div className="App">
-      heeeeyyyyyyyyyyy
+      <LoaderPage />
     </div>
   )
 }
---
+++ src/Pages/LoaderPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LoaderPage = () => {
+    return (
+        <div className="loaderPage">
+            <div className="logoWrapper">
+                
+            </div>
+        </div>
+    )
+}
+
+export default LoaderPage
