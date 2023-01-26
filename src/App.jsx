3043efb937bc60ea66cd45debc6c408a0a52f0cf
unknown <doniaskima344@gmail.com>
2023-01-26 04:37:33 +0100

    add SignupPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import LoaderPage from "./Pages/LoaderPage";
-import Login from "./Pages/Login";
 import "./App.css"
 import "./styles/main.scss"
+import LoaderPage from "./Pages/LoaderPage";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+ 
 function App() {
   return (
-    <div className="App">
+    <div className="">
       <Login />
     </div>
   )
--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import styled from "styled-components";
-import Logo from "../assets/Whatsapp-logo.png"
-const Login = () => {
+import Logo from "../assets/Whatsapp-logo.png";
+
+
+const Signup = () => {
     return (
-        <div className="form-container">
+        <div className="form-container-signup">
             <div className="logo-wattsap">
                 <img src={Logo} alt="wattsap-logo" className="wattsap-logo" />
             </div>
@@ -12,27 +14,46 @@ const Login = () => {
                     Join Wattsap Today
                 </FormTitle>
                 <InputContainer>
-                    <InputLabel
-                        htmlFor="email">
+                    <InputLabel htmlFor="email">
                         Email
                     </InputLabel>
                     <InputField
                         id="email"
                         type="email"
                         placeholder="Your Email"
+                       
+                    />
+                </InputContainer>
+                <InputContainer>
+                    <InputLabel htmlFor="text">FirstName</InputLabel>
+                    <InputField
+                        id="firstName"
+                        type="firstName"
+                        placeholder="Your FirstName"
+                    />
+                </InputContainer>
+                <InputContainer>
+                    <InputLabel htmlFor="text">LastName</InputLabel>
+                    <InputField
+                        id="Lastname"
+                        type="Lastname"
+                        placeholder="Your Name"
+                    />
+                </InputContainer>
+                <InputContainer>
+                    <InputLabel htmlFor="password">Password</InputLabel>
+                    <InputField
+                        id="password"
+                        type="password"
+                        placeholder="Your password"
 
                     />
                 </InputContainer>
                 <InputContainer>
-                    <InputLabel
-                        htmlFor="password"
-                    >
-                        Password
-                    </InputLabel>
-
+                    <InputLabel htmlFor="password">ConfirmPassword</InputLabel>
                     <InputField
-                        id="email"
-                        type="email"
+                        id="password"
+                        type="password"
                         placeholder="Your password"
                     />
                 </InputContainer>
@@ -84,4 +105,4 @@ font-family: 'Syne Mono', monospace;
   text-align: center;
 `
 
-export default Login
+export default Signup
