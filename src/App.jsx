d97973ccd9121c083722951f77a2bc6b97a62163
unknown <doniaskima344@gmail.com>
2023-01-26 04:43:45 +0100

    implement react router

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,16 @@ import "./styles/main.scss"
 import LoaderPage from "./Pages/LoaderPage";
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
- 
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <div className="">
-      <Login />
+      <Routes>
+        <Route path="/" element={<LoaderPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </div>
   )
 }
