aba130fc194b56ab187fb70a466eb82c323f044c
unknown <doniaskima344@gmail.com>
2023-02-03 01:42:42 +0100

    feat(context):add dataProvider

---
+++ src/Context/dataProvider.js
@@ -0,0 +1,113 @@
+import axios from "axios";
+import { createContext, useContext, useEffect, useState } from "react";
+
+
+import {
+    baseUrl,
+    fetchChats,
+    scrollBottom,
+    axiosDelete,
+    deleteSavedMessage,
+} from "../utils/api";
+import { useAuth } from "../authProvider";
+
+const DataContext = createContext();
+
+
+export const DataProvider = ({ children }) => {
+    const { user } = useAuth();
+    const [recipients, setRecipients] = useState([]);
+    const [groups, setGroups] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [messages, setMessages] = useState([]);
+    const [messagesLoading, setMessagesLoading] = useState(false);
+
+    useEffect(() => {
+        const fetch = async () => {
+            try {
+                setLoading(true);
+                const res = await axios.get(`${baseUrl}/users/recipients/${user._id}`);
+                setRecipients(res.data.recipients);
+                setLoading(false);
+                setLoading(true);
+                const secondResponse = await axios.get(`${baseUrl}/users/groups/${user._id}`);
+                setGroups(secondResponse.data.groups);
+                setLoading(false);
+            } catch (err) {
+                console.log(err);
+                setLoading(false);
+            }
+        };
+        fetch();
+    }, []);
+
+
+
+
+    const addRecipient = (item) => {
+        if (recipients.findIndex((r) => r._id === item.sender._id) === -1) {
+            setRecipients((prevData) => [...prevData, item]);
+        }
+    };
+
+    const removeRecipient = (id) => {
+        setRecipients((prevData) => prevData.filter((obj) => obj._id !== id));
+    };
+
+    const addGroup = (id, name, description, isPublic) => {
+        function callback(obj) {
+            if (obj._id === id) {
+                obj.name = name;
+                obj.description = description;
+                obj.isPublic = isPublic;
+            }
+            return obj;
+        }
+        setGroups((prevState) => prevState.map(callback));
+    }
+
+    const removeGroup = (id) => {
+        setGroups((prevData) => prevData.filter((obj) => obj._id !== id));
+    };
+
+    const fetchSavedMessages = async (id) => {
+        setMessagesLoading(true);
+        const { data } = await axios.get(`${baseUrl}/users/savedMessages/${id}`);
+        setMessages(data.savedMessages);
+        setMessagesLoading(false);
+    };
+
+    const messageDeleteHandler = async (msg) => {
+        const id = msg.messageId;
+        const isItSavedMessage = msg.sender ? false : true;
+        setMessages((prevState) => prevState.filter((msg) => msg.messageId !== id));
+        if (!isItSavedMessage) {
+            await axiosDelete("messages", id);
+        } else {
+            await deleteSavedMessage(user, id);
+        }
+    };
+
+    return (
+        <DataContext.Provider
+            value={{
+                loading,
+                recipients,
+                addRecipient,
+                updateGroup,
+                groups,
+                addGroup,
+                removeGroup,
+                removeRecipient,
+                fetchMessages,
+                messagesLoading,
+                messages,
+                addMessageCallback,
+                fetchSavedMessages,
+                messageDeleteHandler,
+            }}
+        >
+            {children}
+        </DataContext.Provider>
+    )
+}
