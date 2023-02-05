1ca72f06ae5c990502846cf11067757cacc2b1f9
unknown <doniaskima344@gmail.com>
2023-02-05 23:04:52 +0100

    feat(component):message componenet

--- src/Components/MessagesPageComponents/Message.jsx
+++ src/Components/MessagesPageComponents/Message.jsx
@@ -9,9 +9,43 @@ const Message = ({ msg, isAdmin, isGroup, messageDeleteHandler }) => {
   const time = dayjs(msg.createdAt).format("h.mm a");
   const decryptedMessage = decryptMessage(msg.key, msg.message, msg.iv);
   const [showMessageOptions, setShowMessageOptions] = useState(false);
+  const isMessageSentByClient = msg?.sender?.name === user.name;
 
   return (
-    <div>Message</div>
+    <div
+      onMouseEnter={() => setShowMessageOptions(true)}
+      onMouseLeave={() => setShowMessageOptions(false)}
+      className="message-component"
+    >
+      <div
+        className={`message_send-by_client  ${isMessageSentByClient
+          ? "message_is_send"
+          : "message_is_not_byclient"
+          }`}
+      >
+        {showMessageOptions && isAdmin && (
+          <i
+            role="button"
+            aria-label="Delete Message"
+            className={`trash ${isMessageSentByClient ? "left" : "right"}
+          `}
+            onClick={() => messageDeleteHandler(msg)}
+          />
+        )}
+        ({
+          isGroup && (
+            <p className="">
+              {!isMessageSentByClient && msg?.sender?.name}
+            </p>
+          )
+        })
+        <div className="decrypted-message">
+          <span className="decrMessage">{decryptedMessage}</span>
+          <span className="time-style">{time}</span>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
