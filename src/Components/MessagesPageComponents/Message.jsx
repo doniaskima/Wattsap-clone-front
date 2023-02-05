8592dcb2dd0cf7d9d7c1f70b5b933813bef18422
unknown <doniaskima344@gmail.com>
2023-02-05 22:19:55 +0100

    break coffe

--- src/Components/MessagesPageComponents/Message.jsx
+++ src/Components/MessagesPageComponents/Message.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import { useAuth } from '../../Context/authProvider';
+import { decryptMessage } from "../../utils/api";
+import dayjs from "dayjs";
 
-const Message = () => {
+const Message = ({ msg, isAdmin, isGroup, messageDeleteHandler }) => {
+  const { user } = useAuth();
+  const time = dayjs(msg.createdAt).format("h.mm a");
+  const decryptedMessage = decryptMessage(msg.key, msg.message, msg.iv);
+  const [showMessageOptions, setShowMessageOptions] = useState(false);
+
   return (
     <div>Message</div>
   )
