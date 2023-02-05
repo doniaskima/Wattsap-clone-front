0f63145e6123c5e7daa98075e4c9078284ae4a1b
unknown <doniaskima344@gmail.com>
2023-02-05 23:23:36 +0100

    wrap app

--- src/Context/socket.jsx
+++ src/Context/socket.jsx
@@ -2,7 +2,6 @@ import socketio from "socket.io-client";
 import { baseUrl } from "../utils/api";
 import React, { useContext } from "react";
 
-
 export const socket = socketio(baseUrl);
-export const socketContext = React.createContext();
-export const useSocket = () => useContext(socketContext);
+export const SocketContext = React.createContext();
+export const useSocket = () => useContext(SocketContext);
--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./Context/authProvider"
+import { AuthProvider } from "./Context/authProvider";
+import { SocketContext, socket } from "./Context/socket";
+
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <SocketContext.Provider value={socket}>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </SocketContext.Provider>
     </BrowserRouter>
   </React.StrictMode>,
 );
