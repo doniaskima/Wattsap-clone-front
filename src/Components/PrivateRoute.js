ef42e56f79975492a2a390dd291764ec5c98f0d9
unknown <doniaskima344@gmail.com>
2023-02-05 21:18:58 +0100

    some changes

--- src/Components/PrivateRoute.js
+++ src/Components/PrivateRoute.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { useAuth } from "../Context/authProvider";
-import { Redirect } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
 
 export function PrivateRoute({ component: Component, ...rest }) {
   const { user } = useAuth();
   return user == null ? (
-    <Redirect from="" to="/" noThrow />
+    <Navigate to="/" replace />
   ) : (
     <Component {...rest} />
   );
 }
 
 export default PrivateRoute
+ 
