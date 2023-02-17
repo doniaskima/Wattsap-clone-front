dbbf5ad58db9740aa9711bbc1ca28b4e4f7de815
unknown <doniaskima344@gmail.com>
2023-02-17 13:28:53 +0100

    delete useless things

--- src/Components/MessagesPageComponents/EmojisComponent.jsx
+++ src/Components/MessagesPageComponents/EmojisComponent.jsx
@@ -1,21 +1,9 @@
 import React, { useState } from "react";
-import Icon from "../LoaderPage/index";
 import Picker from "@emoji-mart/react";
 import data from "@emoji-mart/data";
 
 
-const emojiTabs = [
-  { icon: "recent", label: "Recent emojis", active: true },
-  { icon: "emojiPeople", label: "People emojis", active: false },
-  { icon: "emojiNature", label: "Nature emojis", active: false },
-  { icon: "emojiFood", label: "Food emojis", active: false },
-  { icon: "emojiActivity", label: "Activity emojis", active: false },
-  { icon: "emojiTravel", label: "Travel emojis", active: false },
-  { icon: "emojiObjects", label: "Object emojis", active: false },
-  { icon: "emojiSymbols", label: "Symbol emojis", active: false },
-  { icon: "emojiFlags", label: "Flag emojis", active: false },
-];
-
+ 
 const EmojiTray = ({ showEmojis, newMessage, setNewMessage }) => {
   const addEmoji = (emoji) => {
     setNewMessage(newMessage + emoji);
