6e1c345eeb8740abb4cc97fe04b749854d6cafa5
unknown <doniaskima344@gmail.com>
2023-02-18 20:20:49 +0100

    LogoComponent

--- src/Components/subComponents/LogoComponent.jsx
+++ src/Components/subComponents/LogoComponent.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import styled from "styled-components";
+import { DarkTheme } from '../Themes';
 
-const LogoComponent = () => {
+const Logo = styled.h1`
+display:inline-block;
+font-family: 'Pacifico',cursive;
+position:fixed;
+top:2rem;
+left:2rem;
+z-index:3;
+color: ${props => props.color === 'dark' ? DarkTheme.text : DarkTheme.body};
+`
+
+
+const LogoComponent = (props) => {
     return (
-        <div>LogoComponent</div>
+        <Logo color={props.theme}>
+            WhatsApp
+        </Logo>
     )
 }
 
