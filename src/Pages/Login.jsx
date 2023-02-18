7bf3d49b89134ebca4b41b0219419d04bd885a30
unknown <doniaskima344@gmail.com>
2023-02-18 20:42:09 +0100

    feat(component):add ParticleComponent

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,129 +1,31 @@
-import React, { useState } from 'react'
-import styled from "styled-components";
-import Logo from "../assets/Whatsapp-logo.png"
-import { Link, useNavigate } from "react-router-dom";
-import { useAuth } from "../Context/authProvider";
+
+
+
+import React from 'react'
+import styled, { keyframes, ThemeProvider } from 'styled-components'
+import LogoComponent from '../Components/subComponents/LogoComponent';
+import BigTitle from '../Components/subComponents/BigTitle';
+import { DarkTheme } from '../Components/Themes';
+
+const Box = styled.div`
+background-color: ${props => props.theme.body};
+width: 100vw;
+height:100vh;
+position: relative;
+overflow: hidden;
+`
+
+
 
 
 const Login = () => {
-    const { loginWithUserCredentials, emailValidate } = useAuth();
-    const navigate = useNavigate();
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState("");
-
-    const loginHandler = async (event) => {
-        event.preventDefault();
-        setLoading(true);
-        if (emailValidate(email)) {
-            const { user, message } = await loginWithUserCredentials(email, password);
-            if (user == null) {
-                setError(message);
-                setLoading(false);
-                return;
-            }
-            navigate("/home");
-            return;
-        }
-        setError("Enter Valid Email");
-        setLoading(false);
-    };
-
-
     return (
-        <div className="form-container">
-            <div className="logo-wattsap">
-                <img src={Logo} alt="wattsap-logo" className="wattsap-logo" />
-            </div>
-            <form onSubmit={(event) => loginHandler(event)} className="form-style">
-                <FormTitle>
-                    Join Wattsap Today
-                </FormTitle>
-                {error !== "" &&
-                    <p className="error-style">{error}</p>
-                }
-                <InputContainer>
-                    <InputLabel
-                        htmlFor="email">
-                        Email
-                    </InputLabel>
-                    <InputField
-                        id="email"
-                        type="email"
-                        placeholder="Your Email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-
-                    />
-                </InputContainer>
-                <InputContainer>
-                    <InputLabel
-                        htmlFor="password"
-                    >
-                        Password
-                    </InputLabel>
-                    <InputField
-                        id="password"
-                        type="password"
-                        placeholder="Your password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </InputContainer>
-                <div className="btn-auth">
-                    <button
-                        type="submit"
-                        className="style-btn-auth"
-                    >
-                        {loading ? "logging Up..." : "login"}
-                    </button>
-                </div>
-            </form>
-        </div>
-
-
+        <ThemeProvider theme={DarkTheme}>
+            <Box>
+                <LogoComponent theme='dark' />
+            </Box>
+        </ThemeProvider>
     )
 }
-
-export const InputField = styled.input`
-  background: #202225;
-  outline: none;
-  border: none;
-  color: #fff;
-  font-size: 14px;
-  border-radius: 4px;
-  padding: 8px;
-`
-export const TextAreaField = styled.textarea`
-  background: #202225;
-  outline: none;
-  border: none;
-  color: #fff;
-  font-size: 14px;
-  border-radius: 4px;
-  padding: 8px;
-  width: 100%;
-  height: 150px;
-  resize: none;
-`
-export const InputContainer = styled.div`
-  color: #fff;
-  display: flex;
-  flex-direction: column;
-  gap: 5px;
-`
-export const InputLabel = styled.label`
-  color: #adafb3;
-  font-size: 16px;
-`
-export const FormTitle = styled.h2`
-  color: #fff;
-font-family: 'Syne Mono', monospace;
-  font-size: 22px;
-  font-weight: 700;
-  margin-bottom: 20px;
-  text-align: center;
-`
 
 export default Login
